b3ab8fac0cf7c7f8434df4594e733e2d903bf06f
Jordan Bach <jordanbach@gmail.com>
2015-07-26 11:05:53 -0500

    remove admin interface - will create a new one when ready

--- admin/home.go
+++
@@ -1,9 +0,0 @@
-package admin
-
-import (
-	"net/http"
-)
-
-func (app WebApp) Home(w http.ResponseWriter, r *http.Request) {
-	app.RenderHTML(w, "main", "home", nil)
-}
--- admin/services.go
+++
@@ -1,9 +0,0 @@
-package admin
-
-import (
-	"net/http"
-)
-
-func (app WebApp) ListServices(w http.ResponseWriter, r *http.Request) {
-	app.RenderHTML(w, "main", "services/index", nil)
-}
